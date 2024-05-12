88943450993b209ca34ede0b89548b5a76dc8263
thengyuanyuan <thengyuanyuan@gmail.com>
2024-05-12 19:05:03 +0800

    fix resume.pdf

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -1,2 +1,2 @@
 const String profilePath = "assets/images/profile.jpg";
-const String cvFilePath = "assets/files/thengyuanyuan_cv_20240511.pdf";
+const String cvFilePath = "assets/files/thengyuanyuan_cv.pdf";
